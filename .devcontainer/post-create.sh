ddba43c49f3d9fb1f9fb73fa1bfa42428458930e
Justin Yoo <justin.yoojh@gmail.com>
2022-10-02 10:10:57 +0000

    Update devcontainer

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -21,7 +21,7 @@ rm CascadiaCode.zip
 ## AZURE CLI EXTENSIONS ##
 # Uncomment the below to install Azure CLI extensions
 # extensions=$(az extension list-available --query "[].name" | jq -c -r '.[]')
-extensions="account alias deploy-to-azure eventgrid functionapp logic subscription webapp"
+extensions=(account alias deploy-to-azure eventgrid functionapp logic subscription webapp)
 for extension in $extensions;
 do
     az extension add --name $extension
