5b1e3a9016ad80b5f0f88cfd4f29277b09eec1bb
Justin Yoo <justin.yoojh@gmail.com>
2022-10-03 05:24:18 +0000

    Update devcontainer

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -32,15 +32,19 @@ done
 az bicep install
 
 ## AZURE FUNCTIONS CORE TOOLS ##
-# Uncomment the below to install Azure Functions Core Tools
+# Uncomment the below to install Azure Functions Core Tools. Make sure you have installed node.js
 npm i -g azure-functions-core-tools@4 --unsafe-perm true
 
+## Azurite ##
+# Uncomment the below to install Azurite. Make sure you have installed node.js
+npm install -g azurite
+
 ## AZURE STATIC WEB APPS CLI ##
-# Uncomment the below to install Azure Static Web Apps CLI
+# Uncomment the below to install Azure Static Web Apps CLI. Make sure you have installed node.js
 # npm install -g @azure/static-web-apps-cli
 
 ## AZURE DEV CLI ##
-# Uncomment the below to install Azure Dev CLI
+# Uncomment the below to install Azure Dev CLI. Make sure you have installed Azure CLI and GitHub CLI
 curl -fsSL https://aka.ms/install-azd.sh | bash
 
 ## OH-MY-ZSH PLUGINS & THEMES (POWERLEVEL10K) ##
@@ -82,5 +86,6 @@ sudo chmod +x /usr/local/bin/oh-my-posh
 
 # mkdir $HOME/.config/powershell
 # curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-posh/Microsoft.PowerShell_profile.ps1 > $HOME/.config/powershell/Microsoft.PowerShell_profile.ps1
+# curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-posh/Microsoft.PowerShell_profile.ps1 > $HOME/.config/powershell/Microsoft.VSCode_profile.ps1
 
 # cp $HOME/p10k-with-clock.omp.json $HOME/p10k.omp.json
